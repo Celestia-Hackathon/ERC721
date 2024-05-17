02d238819bb66c1f8e6e77ab15591ca8b6494144
JFernandesLO <134019386+JFernandesLO@users.noreply.github.com>
2024-05-17 10:22:21 -0300

    Update CatCoin.sol

--- contracts/CatCoin.sol
+++ contracts/CatCoin.sol
@@ -4,15 +4,20 @@ pragma solidity ^0.8.8;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import { ERC2771Context } from "@gelatonetwork/relay-context/contracts/vendor/ERC2771Context.sol";
 
-contract CatCoin is ERC20, Ownable, ReentrancyGuard {
+contract CatCoin is ERC20, Ownable, ReentrancyGuard, ERC2771Context {
     
     uint256 public ethToCatRate;
 
     event TokensPurchased(address indexed purchaser, uint256 amount, uint256 rate, string currency);
-    event TokensEarned(address indexed claimer, uint256 amount);
+    event TokensTrasfered(address indexed claimer, uint256 amount);
 
-    constructor(address initialOwner, uint256 _ethToCatRate) ERC20("Cat", "CAT") Ownable(initialOwner) {
+    constructor(address initialOwner, uint256 _ethToCatRate, address trustedForwarder) 
+        ERC20("Cat", "CAT") 
+        Ownable(initialOwner)
+        ERC2771Context(trustedForwarder) 
+    {
         _mint(address(this), 10000 * 10 ** decimals());
         ethToCatRate = _ethToCatRate;
     }
@@ -21,18 +26,24 @@ contract CatCoin is ERC20, Ownable, ReentrancyGuard {
         _mint(to, amount);
     }
 
+    function transferTokens(address recipient, uint256 amount) public returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        emit TokensTrasfered(_msgSender(), amount);
+        return true;
+    }
+
+    function claim(uint256 amount) public {
+        require(balanceOf(address(this)) >= amount, "Not enough CatCoins in the reserve");
+        _transfer(address(this), _msgSender(), amount);
+        emit TokensTrasfered(_msgSender(), amount);
+    }
+
     function buyCatCoinWithETH() public payable nonReentrant {
         uint256 catAmount = (msg.value * ethToCatRate);
         require(balanceOf(address(this)) >= catAmount, "Not enough CatCoins in the reserve");
 
         _transfer(address(this), msg.sender, catAmount);
         emit TokensPurchased(msg.sender, catAmount, ethToCatRate, "ETH");
-    }
-
-     function claim(uint256 amount) public {
-        require(balanceOf(address(this)) >= amount, "Not enough CatCoins in the reserve");
-        _transfer(address(this), msg.sender, amount);
-        emit TokensEarned(msg.sender, amount);
     }
 
     function getEthToCatRate() public view returns(uint256) {
@@ -52,4 +63,17 @@ contract CatCoin is ERC20, Ownable, ReentrancyGuard {
         (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Call failed!");
     }
+
+    function withdramCat(uint256 amount) public onlyOwner {
+        _transfer(address(this), msg.sender, amount);
+    }
+    
+    function _msgSender() internal view override(Context, ERC2771Context) returns (address) {
+        return ERC2771Context._msgSender();
+    }
+
+    function _msgData() internal view override(Context, ERC2771Context) returns (bytes calldata) {
+        return ERC2771Context._msgData();
+    }
 }
+
