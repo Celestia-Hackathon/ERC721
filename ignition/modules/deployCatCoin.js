c4ec82e642c30adbbfe2d85930135222b8a915ee
JFernandesLO <134019386+JFernandesLO@users.noreply.github.com>
2024-05-17 10:38:37 -0300

    Update deployCatCoin.js

--- ignition/modules/deployCatCoin.js
+++ ignition/modules/deployCatCoin.js
@@ -5,12 +5,13 @@ dotenv.config();
 async function main() {
   const CatCoin = await ethers.getContractFactory("CatCoin");
   const ethToCatRate = 1000;
-  const catCoin = await CatCoin.deploy(process.env.PUBLIC_KEY, ethToCatRate);
+  const trustForwarder = '0xc65d82ECE367EF06bf2AB791B3f3CF037Dc0e816';
+  const catCoin = await CatCoin.deploy(process.env.PUBLIC_KEY, ethToCatRate, trustForwarder);
 
   await catCoin.waitForDeployment();
 
   console.log("Cat Coin deployed to:", catCoin.target);
-  await verify(catCoin.target, [process.env.PUBLIC_KEY, ethToCatRate]);
+  await verify(catCoin.target, [process.env.PUBLIC_KEY, ethToCatRate, trustForwarder]);
 
 }
   
