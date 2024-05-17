6ec5465d2e4d27f7658b41a4783409ca52c995d1
JFernandesLO <134019386+JFernandesLO@users.noreply.github.com>
2024-05-17 10:52:40 -0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ import { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
 import CatNFT from '../artifacts/contracts/CatNFT.sol/CatNFT.json';
 
-const NFTContractAddress = '0x56012074492279b3C2a7a13c820c3ee5eFb67e7C';
+const NFTContractAddress = '0x411446C52455A4cAF7b089204Cb1eD5888244ef4';
 
 const provider = new ethers.BrowserProvider(window.ethereum);
 
